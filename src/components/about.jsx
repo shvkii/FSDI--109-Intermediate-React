272e1361c35f73c5347e8b60ff3f9f5ce9dc1861
shvkii <92766830+shvkii@users.noreply.github.com>
2022-01-15 13:26:57 -0800

    Add files via upload

---
+++ src/components/about.jsx
@@ -0,0 +1,13 @@
+
+
+import "./about.css";
+
+const About = () => {
+    return (
+        <div className="about-page">
+            <h1>A simple React online store</h1>
+            <h3>Created by Shakita Thompson</h3>
+        </div>
+    )
+}
+export default About;
